261873889eb820e30ddafcb8a3f520e1cfdbb595
Mandy Mason <mnmason86@gmail.com>
2022-08-29 19:04:55 -0400

    created controller class file, added 'hello' and 'capitalize' routes

---
+++ src/main/java/com/mnmason6/songr/HelloController.java
@@ -0,0 +1,24 @@
+package com.mnmason6.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello";
+    }
+
+    @GetMapping("/capitalize/{word}")
+    public String getCapitalize(@PathVariable String word, Model m){
+        m.addAttribute("word", word.toUpperCase());
+        return "capitalize";
+    }
+
+}
